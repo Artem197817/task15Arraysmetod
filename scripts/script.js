c3eaef0223d094bd717db0b1f353467e7f33e267
Artem <katod1978@yandex.ru>
2024-10-25 00:35:15 +0700

    day two

--- scripts/script.js
+++ scripts/script.js
@@ -19,12 +19,23 @@ $(document).ready(function () {
             persons = response[1];
             specializations = response[2];
 
+            outputPersonList(
+                specializationSkill(
+                    specialization(persons, specializations, 'designer'), 'figma'), ' Дизайнеры Figma');
 
-            specializationSkill(specialization(persons, specializations, 'designer'), 'figma')
-                .forEach(person => {
-                    console.log(getInfo.call(person));
-                })
-            reactDeveloper(persons, 'react');
+
+            someSkillDeveloper(persons, 'react');
+
+            checkAge(persons, 18);
+
+            outputPersonList(
+                salaryPersonalSorted(
+                    timeEmployment(cityPersonal(
+                            specialization(persons, specializations, 'backend'), cities, 'Москва'
+                        ), 'Полная'
+                    )
+                ), 'Backend из Москвы на полный день'
+            )
 
         })
         .catch(error => {
@@ -41,10 +52,19 @@ $(document).ready(function () {
         return `${this.personal.firstName} ${this.personal.lastName}, ${city ? city.name : 'Unknown City'}`;
     }
 
+    function outputPersonList(listPerson, message){
+        console.log(message);
+        listPerson.forEach((person) => {
+            console.log(getInfo.call(person))
+        })
+    }
+
     /** Найдите среди пользователей всех дизайнеров,
      * которые владеют Figma и выведите данные о них в консоль с помощью getInfo.*/
     function specialization(persons, specializations, specialization) {
-        let specializationSearchId = specializations.find(item => item.name.toLowerCase() === specialization).id;
+        let specializationSearchId = specializations
+            .find(item => item.name.toLowerCase() === specialization)
+            .id;
         return persons.filter(person => {
             return person.personal.specializationId === specializationSearchId;
         });
@@ -56,18 +76,73 @@ $(document).ready(function () {
                 return person;
         });
     }
-/** Найдите первого попавшегося разработчика, который владеет React.
- * Выведите в консоль через getInfo данные о нем.*/
 
-function reactDeveloper(persons, skill) {
-    const developer = persons.find(person =>
-        person.skills.some(item => item.name.toLowerCase() === skill.toLowerCase()));
+    /** Найдите первого попавшегося разработчика, который владеет React.
+     * Выведите в консоль через getInfo данные о нем.*/
 
-    if (developer) {
-        console.log(getInfo.call(developer));
-    } else {
-        console.log('No developer found with the specified skill.');
+    function someSkillDeveloper(persons, skill) {
+        const developer = persons.find(person =>
+            person.skills
+                .some(item => item.name.toLowerCase() === skill.toLowerCase()));
+
+        if (developer) {
+            console.log(skill)
+            console.log(getInfo.call(developer));
+        } else {
+            console.log(skill)
+            console.log('No developer found with the specified skill.');
+        }
     }
+/** Проверьте, все ли пользователи старше 18 лет. Выведите результат проверки в консоль.*/
+function checkAge(persons, controlAge){
+    isAge = false;
+    persons.forEach(person => {
+        if (calculateAge(person.personal.birthday) <= controlAge) isAge = true;
+    })
+    console.log(isAge? 'Не все пользователи старше ' + controlAge + ' лет': 'Все пользователи старше ' + controlAge + ' лет')
+
+}
+
+    function calculateAge(birthday) {
+
+        const [day, month, year] = birthday.split('.').map(Number);
+        const birthDate = new Date(year, month - 1, day);
+        const today = new Date();
+
+        let age = today.getFullYear() - birthDate.getFullYear();
+
+        const hasBirthdayPassed = today.getMonth() > birthDate.getMonth() ||
+            (today.getMonth() === birthDate.getMonth() && today.getDate() >= birthDate.getDate());
+
+        if (!hasBirthdayPassed) {
+            age--;
+        }
+
+        return age;
+    }
+/**Найдите всех backend-разработчиков из Москвы, которые ищут работу на полный день
+ * и отсортируйте их в порядке возрастания зарплатных ожиданий. */
+
+
+function cityPersonal(listPersonal, cities, cityName) {
+    let cityId = cities.find(item => item.name === cityName).id;
+    return listPersonal.filter(person => {
+        return person.personal.locationId === cityId;
+    });
+}
+
+function salaryPersonalSorted (listPersonal) {
+    function salaryPerson(person){
+        return +(person.request.find(item => item.name === 'Зарплата').value);
+    }
+    return listPersonal.sort((a, b) => salaryPerson(a) - salaryPerson(b));
+}
+
+function timeEmployment(listPersonal, type) {
+
+    return listPersonal.filter(person => {
+        return person.request.find(item => item.name === 'Тип занятости').value === type;
+    })
 }
 
 
